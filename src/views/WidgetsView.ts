f7b2ba037de63658f2df9f8eceef49b475957496
bingryan <41174435+bingryan@users.noreply.github.com>
2022-12-23 05:25:48 +0800

    feat: add calendar (#8)
    * add calendar

--- src/views/WidgetsView.ts
+++ src/views/WidgetsView.ts
@@ -1,13 +1,15 @@
-import { ItemView, WorkspaceLeaf, } from 'obsidian';
+import { ItemView, WorkspaceLeaf, Notice } from 'obsidian';
 import { createApp, App } from 'vue';
 import Pomodoro from '@/components/Pomodoro.vue';
+import Calendar from '@/components/Calendar.vue';
 import WidgetsPlugin from '@/plugin'
 
 export const Widgets_ICON: string = "clock";
 export const Widgets_VIEW_TYPE: string = 'widgets';
 
 export class WidgetsView extends ItemView {
-	vueApp: App;
+	calendarApp: App;
+	pomodoroApp: App;
 	plugin: WidgetsPlugin;
 	constructor(leaf: WorkspaceLeaf, plugin: WidgetsPlugin) {
 		super(leaf);
@@ -23,16 +25,25 @@ export class WidgetsView extends ItemView {
 		return Widgets_ICON;
 	}
 	async onOpen() {
-		// pomodoro
-		const pomodoroContainer = this.containerEl.children[1]; // view-content
-		let content = pomodoroContainer.createDiv({ cls: "pomodoro" });
-		this.vueApp = createApp(Pomodoro);
-		this.vueApp.config.globalProperties.container = content;
-		this.vueApp.config.globalProperties.plugin = this.plugin;
-		this.vueApp.mount(content);
-		// add other widget
+
+		const widgetContainer = this.containerEl.children[1];
+
+		let calendarEl = widgetContainer.createDiv({ cls: "calendar" });
+		this.calendarApp = createApp(Calendar);
+		this.calendarApp.config.globalProperties.container = calendarEl;
+		this.calendarApp.config.globalProperties.plugin = this.plugin;
+		this.calendarApp.mount(calendarEl);
+
+
+		let pomodoroEl = widgetContainer.createDiv({ cls: "pomodoro" });
+		this.pomodoroApp = createApp(Pomodoro);
+		this.pomodoroApp.config.globalProperties.container = pomodoroEl;
+		this.pomodoroApp.config.globalProperties.plugin = this.plugin;
+		this.pomodoroApp.mount(pomodoroEl);
 	}
+
 	async onClose() {
-		this.vueApp.unmount();
+		this.calendarApp.unmount();
+		this.pomodoroApp.unmount();
 	}
 }
